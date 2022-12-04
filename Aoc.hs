db17fd8eab8912bea13f8c7077d33ef88d434c96
NotAPenguin0 <michiel.vandeginste@gmail.com>
2022-12-04 19:30:06 +0100

    day 4

--- Aoc.hs
+++ Aoc.hs
@@ -7,3 +7,14 @@ first2 _ = error "Invalid element count"
 
 solve :: (a -> Int) -> (a -> Int) -> a -> (Int, Int)
 solve p1 p2 input = first2 $ [p1, p2] <*> [input]
+
+-- applies a function to a tuple of 2 variables
+mapTuple :: (a -> b) -> (a, a) -> (b, b)
+mapTuple f (x, y) = (f x, f y)
+
+-- applies a tuple of 2 functions to 2 variables
+applyTuple :: (a -> b) -> (c -> d) -> a -> c -> (b, d)
+applyTuple f1 f2 x y = (f1 x, f2 y)
+
+applyTuple2 :: (a -> b -> c) -> (a -> b -> d) -> a -> b -> (c, d)
+applyTuple2 f1 f2 x y = (f1 x y, f2 x y)
---
+++ day4.hs
@@ -0,0 +1,34 @@
+import Aoc
+import System.IO
+import Data.List
+import Data.List.Split
+
+data Interval = Interval Int Int
+    deriving (Eq, Show)
+
+contains :: Interval -> Interval -> Bool
+contains (Interval s1 e1) (Interval s2 e2) = s1 <= s2 && e1 >= e2
+
+partialContains :: Interval -> Interval -> Bool
+partialContains (Interval s1 e1) (Interval s2 e2) = (s1 <= s2 && s2 <= e1)
+
+oneContains :: (Interval -> Interval -> Bool) -> Interval -> Interval -> Bool
+oneContains f x y = (uncurry (||)) $ (applyTuple2 (f) (flip f) x y)
+
+part1 :: [(Interval, Interval)] -> Int
+part1 = length . filter (uncurry $ (oneContains contains))
+
+part2 :: [(Interval, Interval)] -> Int
+part2 = length . filter (uncurry $ (oneContains partialContains))
+
+parse :: String -> [(Interval, Interval)]
+parse = (map . mapTuple) 
+            ((uncurry Interval) . mapTuple (read) . first2 . splitOn "-") 
+        . map (first2 . splitOn ",")
+         . lines
+
+main :: IO ()
+main = 
+    openFile "input/day4.txt" ReadMode
+    >>= hGetContents
+    >>= print . solve part1 part2 . parse
